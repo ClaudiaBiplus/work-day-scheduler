dec0940db17decea2f5c27da7e2387d877154187
ClaudiaBiplus <claudia.filannino@gmail.com>
2024-03-23 22:01:52 +0000

    added localStorage memory

--- assets/script.js
+++ assets/script.js
@@ -27,14 +27,25 @@ $('.time-block').each(function() {
         $(this).removeClass('present')
         $(this).removeClass('future')
     }
-    
 
 })
 
 
-
-//Save the event in local storage when the save button is clicked in that timeblock.
-
-
-
-
+    //grabs values from time and value divs and saves them to local storage
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+      });
+    
+    //retrieves items from local storage and sets them in proper places
+    $("#09 .description").val(localStorage.getItem("09"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
